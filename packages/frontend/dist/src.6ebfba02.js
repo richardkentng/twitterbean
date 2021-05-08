034d7698f088ae9f513369ab45e9d7b9149fa39f
Richard Ng <richardkentng@gmail.com>
2021-05-08 15:43:30 -0700

    dist folder changes from submain pull

--- packages/frontend/dist/src.6ebfba02.js
+++ packages/frontend/dist/src.6ebfba02.js
@@ -88439,13 +88439,6 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function ProfilePage() {
-  //     const [state, dispatch] = useContext(Context)
-  console.log(">>>>>>>");
-  console.log(_StateProvider.StateContext);
-  console.log("<<<<<<"); //     useEffect(() => {
-  // console.log(state)
-  //     }, []);
-
   return _react.default.createElement("div", null, _react.default.createElement("h1", null, " You've reached your Profile Page"));
 }
 },{"react":"node_modules/react/index.js","../StateProvider":"src/StateProvider.tsx"}],"src/App.jsx":[function(require,module,exports) {
@@ -89313,7 +89306,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63520" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55338" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

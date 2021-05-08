4bb551a2433ef821a89ce88e63d0a633ba48a5ed
BernardaP <btp210@gmail.com>
2021-05-08 17:30:15 -0600

    working branch ready for merge

--- packages/frontend/dist/src.6ebfba02.js
+++ packages/frontend/dist/src.6ebfba02.js
@@ -88432,23 +88432,38 @@ exports.default = ProfilePage;
 
 var _react = _interopRequireWildcard(require("react"));
 
+var _authApi = require("../auth/authApi");
+
 var _StateProvider = require("../StateProvider");
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function ProfilePage() {
-  //     const [state, dispatch] = useContext(Context)
-  console.log(">>>>>>>");
-  console.log(_StateProvider.StateContext);
-  console.log("<<<<<<"); //     useEffect(() => {
-  // console.log(state)
-  //     }, []);
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr && (typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]); if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function ProfilePage() {
+  var _useState = useState([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      user = _useState2[0],
+      setUser = _useState2[1];
+
+  (0, _react.useEffect)(function () {
+    (0, _authApi.checkSession)().then(function (user) {});
+  });
   return _react.default.createElement("div", null, _react.default.createElement("h1", null, " You've reached your Profile Page"));
 }
-},{"react":"node_modules/react/index.js","../StateProvider":"src/StateProvider.tsx"}],"src/App.jsx":[function(require,module,exports) {
+},{"react":"node_modules/react/index.js","../auth/authApi":"src/auth/authApi.ts","../StateProvider":"src/StateProvider.tsx"}],"src/App.jsx":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -89313,7 +89328,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63520" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49778" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- packages/frontend/src/profile/ProfilePage.js
+++ packages/frontend/src/profile/ProfilePage.js
@@ -1,10 +1,19 @@
 
 import React, {useContext, useEffect} from 'react'
+import { checkSession } from '../auth/authApi'
 import {StateContext} from '../StateProvider'
 
 
 
 export default function ProfilePage() {
+    const [user, setUser] = useState([]);
+   
+    useEffect(() => {
+        checkSession().then((user) => {
+
+        })
+    })
+
 
 
     return (
